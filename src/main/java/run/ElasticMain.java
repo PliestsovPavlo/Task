f04a1076fe21c407440924d4ac0a53db7a5e7da1
psp <pspalfa@gmail.com>
2017-07-05 16:54:11 +0300

    last

--- src/main/java/run/ElasticMain.java
+++ src/main/java/run/ElasticMain.java
@@ -13,31 +13,6 @@ public class ElasticMain {
 
 	public static void main(String[] args) throws IOException {
 
-		
-		
-//		List<String>list = new ArrayList<>();
-//		for (int i = 0; i < 10; i++) {
-//			list.add(redis.lpop("aaa"));
-//		}
-		
-		
-//		elasticHandler.createIndex("some_index");
-//		System.out.println("written");
-		
-//		elasticHandler.searchIndex("some_index");
-//		elasticHandler.addToIndex("some_index", "test", "6", json);
-		
-//		for (Integer i = 0; i < 10; i++) {
-//			
-//			elasticHandler.addToIndex("some_index", "test", i.toString(), json);
-//		}
-		
-//		elasticHandler.searchById("some_index", "test", "3");
-//		elasticHandler.deleteByIndexById("some_index", "test", "2");
-//		elasticHandler.deleteIndexByName("some_index");
-		
-//		elasticHandler.closeConnection();
-		
 		new Thread(ElasticHandlerImpl.getInstance()).run();
 		new Thread(ElasticHandlerImpl.getInstance()).run();
 		
